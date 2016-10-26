21af0d79936397dcbbc6fabad9508371a706b81f
NathanHaim <nathan.haim@free.fr>
2016-10-26 11:08:01 +0200

    Restructuration swish.pl

--- core/swish.pl
+++ core/swish.pl
@@ -1,7 +1,5 @@
 :-module(main,[wall/1,path/1,bomb/1,block/1,accessible/3,move/5,movements/4,updateList/5]).
 
-:- dynamic board/1.
-:- dynamic playersList/1.
 
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
@@ -14,116 +12,12 @@
 :- http_handler(root(beat), beat,[]).
 
 
+%%%%%%%%%%%%%%%% Server Side %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
 server(Port) :-						% (2)
 http_server(http_dispatch, [port(Port)]).
-
-
-
-%:- dynamic display/1.
-wall('x').
-path('_').
-bomb('b').
-
-block('x').
-block('o').
-block('b').
-
-% Function : Accessible
-% Objectif : Savoir si le point est acessible pour un joueur ou non
-% Retour : Si le point est égal a 'x' ou 'o' la fonction
-%         retourne false sinon true
-accessible(Board,X,Y) :- nth0(Y,Board,Line), nth0(X,Line,Point), not(block(Point)).
-
-% Function : Movements
-% Objectif : Connaître si un mouvement est possible pour un joueur ou non
-% Parameter 1: x-axis Player
-% Parameter 2: y-axis Player
-% Parameter 3: x-axis Destination
-% Parameter 4: y-axis Destination
-% Retour : Si il peut true sinon false
-movements(Xp,Y,Xd,Y) :- Xp is Xd+1; Xd is Xp+1.
-movements(X,Yp,X,Yd) :- Yp is Yd+1; Yd is Yp+1.
-
-% Function : DefineBoard
-% Objective : Define a specific board 
-% Retour : True pour valider le changement de board
-defineBoard(Board) :- assert(board(Board)).
-
-%Liste des mouvements disponible dans le jeu
-%Up 
-move(0,X,Y,NewX,NewY):- NewX = X,NewY is Y-1.    
-%Rigth
-move(1,X,Y,NewX,NewY):- NewX is X+1,NewY = Y. 
-%Down
-move(2,X,Y,NewX,NewY):- NewX = X,NewY is Y+1. 
-%Left
-move(3,X,Y,NewX,NewY):- NewX is X-1,NewY = Y. 
-
-%replace(NewElem,Index,List,NewList):- 
-replace(Index,Current):- Index =:= Current ->
-    writeln('X = Current');   writeln('X').
-% V1.0 : Mouvement Aléatoire sur le plateau sans attaque
-ia(X,Y,NewX,NewY):-repeat, random_between(0,4,Move),move(Move,X,Y,NewX,NewY),board(Board),accessible(Board,NewX,NewY),!.
-
-% MouvementPlayer : update PlayerList with new players coordinates.
-mouvementPlayer(NumPlayer, NewX, NewY) :- playersList(List),
-    updateList(NumPlayer,List, NewList, NewX,NewY),
-    retract(playersList(List)),
-    assert(playersList(NewList)).
-                 
-% Replace [X,Y] in a coordinates list. listeMaj(Index,OldList,NewList,NewX,NewT).
-updateList(0,[_|H],[[NewX,NewY]|H],NewX,NewY).                                                                        
-updateList(Index,[L|H],[L|H2], NewX, NewY):- N is Index-1, updateList(N,H,H2,NewX,NewY).
-
-% createMap :
-% Retour : La carte de jeu
-createMap(X):- X =[
-       	  ['x','x','x','x','x','x','x','x','x'],
-          ['x','A','_','_','_','_','_','B','x'],
-          ['x','_','x','_','x','_','x','_','x'],
-          ['x','_','_','_','_','_','_','_','x'],
-          ['x','_','x','_','x','_','x','_','x'],
-          ['x','_','_','_','_','_','_','_','x'],
-          ['x','_','x','_','x','_','x','_','x'],
-          ['x','C','_','_','_','_','_','D','x'],
-          ['x','x','x','x','x','x','x','x','x']
-         ].
-
-play:- 	playersList(ListPlayer),
-    	playersBeat(0, ListPlayer),
-    	displayBoard,
-    	writeln('PositionJoueur: '),
-    	write(ListPlayer).
-
-
-playHtml :-
-    playersList(ListPlayer),
-    playersBeat(0, ListPlayer),
-    displayBoard,
-    writeln('PositionJoueur: '),
-    reply_html_page(title('Bomberman'),[p(write(ListPlayer))]).
-
-
-%displayPlayerList([]).
-%displayPlayerList([H|T]):-writeln(''), displayLine(H), displayPlayerList(T).
-                     
-playersBeat(_,[]).
-playersBeat(Index,[[X,Y]|T]):-ia(X,Y,NewX,NewY),
-    mouvementPlayer(Index, NewX, NewY),
-    N is Index+1, playersBeat(N,T).
-
-% createMap :
-% Objectif : Affiche la carte du jeu en allant chercher la variable globale
-displayBoard:- board(Board),display(Board).
-
-% createMap :
-% Objectif : Affiche la carte du jeu
-displayLine([]).
-displayLine([H|T]):-write(H), displayLine(T).
-display([]).
-display([Head|Tail]):-writeln(''),displayLine(Head),display(Tail).
-
-test:- createMap(Board),assert(board(Board)),ia(1,1).
 
 init(_Request):- 	createMap(Board),
     	assert(board(Board)),
@@ -135,10 +29,79 @@ beat(_Request) :- playersList(ListPlayer),
                 playersBeat(0, ListPlayer),
                 reply_json(json([list=ListPlayer])).
 
+playHtml :-
+    playersList(ListPlayer),
+    playersBeat(0, ListPlayer),
+    displayBoard,
+    writeln('PositionJoueur: '),
+    reply_html_page(title('Bomberman'),[p(write(ListPlayer))]).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%:- dynamic display/1.
+
+%%%%%%%%%%%%%%%% Dynamic and Static  predicats %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- dynamic board/1.
+:- dynamic playersList/1.
+
+wall('x').
+path('_').
+bomb('b').
+
+block('x').
+block('o').
+block('b').
+
+%Liste des mouvements disponible dans le jeu
+%Up 
+move(0,X,Y,NewX,NewY):- NewX = X,NewY is Y-1.    
+%Rigth
+move(1,X,Y,NewX,NewY):- NewX is X+1,NewY = Y. 
+%Down
+move(2,X,Y,NewX,NewY):- NewX = X,NewY is Y+1. 
+%Left
+move(3,X,Y,NewX,NewY):- NewX is X-1,NewY = Y. 
 
 
-%%%%%%%%%%%%% Search if a case is dangerous	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Function : Movements
+% Objectif : Connaître si un mouvement est possible pour un joueur ou non
+% Parameter 1: x-axis Player
+% Parameter 2: y-axis Player
+% Parameter 3: x-axis Destination
+% Parameter 4: y-axis Destination
+% Retour : Si il peut true sinon false
+movements(Xp,Y,Xd,Y) :- Xp is Xd+1; Xd is Xp+1.
+movements(X,Yp,X,Yd) :- Yp is Yd+1; Yd is Yp+1.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Tools %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Function : Accessible
+% Objectif : Savoir si le point est acessible pour un joueur ou non
+% Retour : Si le point est égal a 'x' ou 'o' la fonction
+%         retourne false sinon true
+accessible(Board,X,Y) :- nth0(Y,Board,Line), nth0(X,Line,Point), not(block(Point)).
+
+
+% Function : DefineBoard
+% Objective : Define a specific board 
+% Retour : True pour valider le changement de board
+defineBoard(Board) :- assert(board(Board)).
+
+
+% Replace [X,Y] in a coordinates list. listeMaj(Index,OldList,NewList,NewX,NewT).
+updateList(0,[_|H],[[NewX,NewY]|H],NewX,NewY).                                                                        
+updateList(Index,[L|H],[L|H2], NewX, NewY):- N is Index-1, updateList(N,H,H2,NewX,NewY).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%% IA Side %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% V1.0 : Mouvement Aléatoire sur le plateau sans attaque
+ia(X,Y,NewX,NewY):-repeat, random_between(0,4,Move),move(Move,X,Y,NewX,NewY),board(Board),accessible(Board,NewX,NewY),!.
+
+
+% Function  Search if a case is dangerous -> see danger()	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Search for a particular bomb ... / Call by dangerParBombPlayer
 dangerPerBomb(X,Y,H):- nth0(0,H,XBomb), nth0(1,H,YBomb), nth0(3,H,Puissance), ((      X=:= XBomb, (Val is  YBomb-Y, Val >= 0 ; Val is Y-YBomb, Val >=0)); (   Y=:= YBomb, (Val is  XBomb-X, Val >= 0 ; Val is X-XBomb, Val >=0))), Puissance >= Val .
 
@@ -155,3 +118,73 @@ dangerPerBombPlayer(X,Y,[H|T]):- dangerPerBomb(X,Y,H); dangerPerBombPlayer(X,Y,T
 danger(X,Y,[H|T]):- dangerPerBombPlayer(X,Y,H); danger(X,Y,T).
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% Game Engine %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% MouvementPlayer : update PlayerList with new players coordinates.
+mouvementPlayer(NumPlayer, NewX, NewY) :- playersList(List),
+    updateList(NumPlayer,List, NewList, NewX,NewY),
+    retract(playersList(List)),
+    assert(playersList(NewList)).
+                 
+
+
+
+% A Game turn
+play:- 	playersList(ListPlayer),
+    	playersBeat(0, ListPlayer),
+    	displayBoard,
+    	writeln('PositionJoueur: '),
+    	write(ListPlayer).
+
+
+
+
+
+
+% Instant T movement all players
+% Parameter 1 : Index of player
+% Parameter 2 : The list of player                    
+playersBeat(_,[]).
+playersBeat(PlayerIndex,[[X,Y]|T]):-ia(X,Y,NewX,NewY),
+    mouvementPlayer(PlayerIndex, NewX, NewY),
+    N is PlayerIndex+1, playersBeat(N,T).
+
+% Display Board
+% Aim : Affiche la carte du jeu en allant chercher la variable globale
+displayBoard:- board(Board),display(Board).
+
+% Display Line :
+% Aim : Show each line of the map
+displayLine([]).
+displayLine([H|T]):-write(H), displayLine(T).
+display([]).
+display([Head|Tail]):-writeln(''),displayLine(Head),display(Tail).
+
+
+% createMap :
+% Retour : La carte de jeu
+createMap(X):- X =[
+       	  ['x','x','x','x','x','x','x','x','x'],
+          ['x','A','_','_','_','_','_','B','x'],
+          ['x','_','x','_','x','_','x','_','x'],
+          ['x','_','_','_','_','_','_','_','x'],
+          ['x','_','x','_','x','_','x','_','x'],
+          ['x','_','_','_','_','_','_','_','x'],
+          ['x','_','x','_','x','_','x','_','x'],
+          ['x','C','_','_','_','_','_','D','x'],
+          ['x','x','x','x','x','x','x','x','x']
+         ].
+
+%displayPlayerList([]).
+%displayPlayerList([H|T]):-writeln(''), displayLine(H), displayPlayerList(T).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
