ef152ca93e3bb2e3c0080bd8c4d9d341930830f3
Théophile Sayer <Theophile@macbook-air-de-theophile.local>
2016-11-03 15:30:34 +0100

    kfet is good

--- core/swish.pl
+++ core/swish.pl
@@ -499,25 +499,21 @@ branch(4,X,Y,Board,PlayerList,BombList,ValueGlobal) :-
 branch(It,X,Y,Board,PlayerList,BombList,ValueGlobal) :-
 
     Index is It+1,
-    branch(Index,X,Y,Value0),
-    Y0 is Y-1,branch(Index,X,Y0,Value1),
-    X0 is X+1,branch(Index,X0,Y,Value2),
-    Y1 is Y+1,branch(Index,X,Y1,Value3),
-    X1 is X-1,branch(Index,X1,Y,Value4),
+    branch(Index,X,Y,Board,BombList,PlayerList,Value0),
+    Y0 is Y-1,branch(Index,X,Y0,Board,BombList,PlayerList,Value1),
+    X0 is X+1,branch(Index,X0,Y,Board,BombList,PlayerList,Value2),
+    Y1 is Y+1,branch(Index,X,Y1,Board,BombList,PlayerList,Value3),
+    X1 is X-1,branch(Index,X1,Y,Board,BombList,PlayerList,Value4),
     correspondingWeightOfCoordinate(X,Y,Board,PlayerList,BombList,WheightValue),
     ValueGlobal is Value1 + Value2 + Value3 + Value4 + Value0 + WheightValue.
 
 
 createPonderatedList(X,Y,Board,BombList,PlayerList,List) :-
-    A is Y-1,
-    B is X+1,
-    C is Y+1,
-    D is X-1,
     branch(0,X,Y,Board,BombList,PlayerList,Value1),
-    branch(0,X,A,Board,BombList,PlayerList,Value2),
-    branch(0,B,Y,Board,BombList,PlayerList,Value3),
-    branch(0,X,C,Board,BombList,PlayerList,Value4),
-    branch(0,Ds,Y,Board,BombList,PlayerList,Value5),
+    A is Y-1,branch(0,X,A,Board,BombList,PlayerList,Value2),
+    B is X+1,branch(0,B,Y,Board,BombList,PlayerList,Value3),
+    C is Y+1,branch(0,X,C,Board,BombList,PlayerList,Value4),
+    D is X-1,branch(0,D,Y,Board,BombList,PlayerList,Value5),
     List = [Value1,Value2,Value3,Value4,Value5].
 
 
@@ -765,7 +761,8 @@ playersBeat(_, NewBoard, [], NewBombs, NewBoard, [], NewBombs):- ! .
 playersBeat(IndexPlayer, Board, [Player|T], ListBombs, NewBoard, [NewPlayer|NewT], NewListBombs):-
 	nth0(5, Player, IA),
 	%ia(IA, Player, Board, ListBombs, Bomb, Direction),
-	iaAggresive(IndexPlayer,Bomb,Direction),
+	%iaAggresive(IndexPlayer,Bomb,Direction),
+    ia(2,IndexPlayer,ListPlayer,Board,BombList,NextMove),
 	applyAction(IndexPlayer, Board, Player, ListBombs, Direction, Bomb, NewPlayer, TListBombs, TBoard),
 	NewIndexPlayer is IndexPlayer+1,
 	playersBeat(NewIndexPlayer, TBoard, T, TListBombs, NewBoard, NewT, NewListBombs).
