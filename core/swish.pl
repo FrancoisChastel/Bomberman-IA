ca43e24cc16672b04dddcb3050dca10f95ead6bf
antoinepay <antoine.payan@hotmail.fr>
2016-11-04 11:45:57 +0100

    Modif IA Aggressive

--- core/swish.pl
+++ core/swish.pl
@@ -409,21 +409,21 @@ ia(1,IndexPlayer,PlayersList,Board,BombList,Bomb,NextMove):-
 
   % ------------------------------------
   
-    p_danger(danger(X,Y,BombList),Board,BombList,Bomb,NextMove),!.
+    p_danger(danger(X,Y,BombList),Board,BombList,X,Y,TargetX,TargetY,Power,Bomb,NextMove),!.
 
 
 %escapeBomb(X,Y,NextMove):-repeat, board(Board), random_between(0,3,NextMove),move(NextMove,X,Y,NewX,NewY),accessible(Board,NewX,NewY),!.
 
-p_danger(Fonction,Board,BombList,X,Y,Bomb,NextMove):-Fonction,	
+p_danger(Fonction,Board,BombList,X,Y,_,_,_,Bomb,NextMove):-Fonction,	
           % Danger : Move to safe place
           backToSafePlace(X,Y,Board,BombList,[],5,Safe,Move),
-          %move(Move,X,Y,NX,NY),
-	  %isBomb(NX,NY,BombList,0),
+          move(Move,X,Y,NX,NY),
+	  isBomb(NX,NY,BombList,0),
           actionSafe(Board,X,Y,Safe,Bomb,Move,NextMove).
 
-p_danger(_,Board,BombList,X,Y,Bomb,NextMove):-
+p_danger(_,Board,BombList,X,Y,TargetX,TargetY,Power,Bomb,NextMove):-
 	    %No Danger
-	    p_LineOfFire(lineOfFire(X,Y,TargetX,TargetY,Power),Board,BombList,X,Y,Bomb,NextMove).
+	    p_LineOfFire(lineOfFire(X,Y,TargetX,TargetY,Power),Board,BombList,X,Y,TargetX,TargetY,Bomb,NextMove).
 
  
 % Called By IaAggresive
@@ -456,22 +456,23 @@ actionSafe(Board,X,Y,0,Bomb,_,NextMove):-
 
 % Target in line of Fire
 % For the next version implement better move after drop bomb
-p_LineOfFire(Fonction,Board,_,X,Y,Bomb,NextMove):-
+p_LineOfFire(Fonction,Board,_,X,Y,_,_,Bomb,NextMove):-
 			   Fonction,
 			   dropBomb(X,Y,Board,Bomb),
 			   NextMove = -1,!.
 
               % No Ennemi in Line of Fire
               % Check Close Object
-p_LineOfFire(_,Board,BombList,X,Y,Bomb,NextMove):-
+p_LineOfFire(_,Board,BombList,X,Y,TargetX,TargetY,Bomb,NextMove):-
 		           checkCloseObject(Board,BombList,X,Y,Find,Move),
 			   closeObjectDetected(Find,Board,X,Y,TargetX,TargetY,Bomb,Move,NextMove),!.
 				
 
 %Called By IaAggresive  
 %------------------------------------------------  
-closeObjectDetected(1,_,_,_,_,_,_,Move,NextMove):-
-            NextMove = Move.
+closeObjectDetected(1,_,_,_,_,_,Bomb,Move,NextMove):-
+            NextMove = Move,
+	    Bomb = 0.
 
 closeObjectDetected(_,Board,X,Y,TargetX,TargetY,Bomb,_,NextMove):-
     checkSafeAndAttainableSquareAroundPlayer(X,Y,SquareList),
